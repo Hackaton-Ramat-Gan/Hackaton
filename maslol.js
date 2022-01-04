2d63c82c1700e1c8b68e8c2c6501b7c76b449543
linoy1 <linoysimantov@se.shenkar.ac.il>
2022-01-04 15:05:52 +0200

    change files name

--- maslol.js
+++ maslol.js
@@ -1,11 +1,11 @@
 const selectedLocations = [
-    {title: 'אולם ספורט מרום נווה', order: 1, videoUrl: 'assets/videos/1.mp4', latlng: { lat: 32.073191, lng: 34.832483 }},
-    {title: 'רולדין', order: 2, latlng: { lat: 32.07176, lng: 34.82921 }},
-    {title: 'פארק מרום נווה', order: 3, latlng: { lat: 32.07169, lng: 34.83081 }},
-    {title: 'מרכז נהורה', order: 4, latlng: { lat: 32.07204, lng: 34.82907 }},
-    {title: 'קניון עופר', order: 5, latlng: { lat: 32.07159, lng: 34.82826 }},
-    {title: 'מן בורגר', order: 6, latlng: { lat: 32.07215, lng: 34.82893 }},
-    {title: 'גינת כלבים', order: 7, latlng: { lat: 32.07096, lng: 34.82924 }},
+    { title: 'פוטו נגבה', order: 1, videoUrl: 'assets/videos/1.mp4', latlng: { lat: 32.07022, lng: 34.8275 } },
+    { title: 'רולדין', order: 2, latlng: { lat: 32.07176, lng: 34.82921 } },
+    { title: 'פארק מרום נווה', order: 3, latlng: { lat: 32.07169, lng: 34.83081 } },
+    { title: 'מרכז נהורה', order: 4, latlng: { lat: 32.07204, lng: 34.82907 } },
+    { title: 'קניון עופר', order: 5, latlng: { lat: 32.07159, lng: 34.82826 } },
+    { title: 'מן בורגר', order: 6, latlng: { lat: 32.07215, lng: 34.82893 } },
+    { title: 'גינת כלבים', order: 7, latlng: { lat: 32.07096, lng: 34.82924 } },
 ];
 
 let map;
@@ -49,22 +49,22 @@ const setRoute = () => {
 
         const directionsRenderer = new google.maps.DirectionsRenderer();
         directionsRenderer.setMap(map);
-        directionsRenderer.setOptions({suppressMarkers: true});
+        directionsRenderer.setOptions({ suppressMarkers: true });
 
         directionsService.route({
-            origin: location.latlng,
-            destination: selectedLocations[index + 1].latlng,
-            travelMode: google.maps.TravelMode.WALKING,
-        })
-        .then((response) => {
-          directionsRenderer.setDirections(response);
-        })
-        .catch((e) => window.alert("Directions request failed due to " + e.status));
+                origin: location.latlng,
+                destination: selectedLocations[index + 1].latlng,
+                travelMode: google.maps.TravelMode.WALKING,
+            })
+            .then((response) => {
+                directionsRenderer.setDirections(response);
+            })
+            .catch((e) => window.alert("Directions request failed due to " + e.status));
     });
 };
 
 $(document).ready(() => {
-    selectedLocations.sort(({order: firstOrder}, {order: secondOrder}) => firstOrder - secondOrder);
+    selectedLocations.sort(({ order: firstOrder }, { order: secondOrder }) => firstOrder - secondOrder);
 
     initMap();
     setMarkers();
